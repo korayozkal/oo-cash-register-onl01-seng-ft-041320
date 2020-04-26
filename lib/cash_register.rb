b5393a4c98cae18e1b05565da442d50bbf651daa
Koray Ozkal <koryozkal@gmail.com>
2020-04-26 23:32:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,8 @@ def initialize(discount=0)
   end	 
   
   def add_item(title, amount, quantity=1)
-    self.total += price * quantity	    self.total += amount * quantity
+    self.total += price * quantity	    
+    self.total += amount * quantity
     quantity.times { self.items << title }	    quantity.times do
     self.last_transaction = [title, price, quantity]	      items << title
     end
