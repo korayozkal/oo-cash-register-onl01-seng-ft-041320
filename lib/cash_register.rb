6138bde186c4c21e873313d56710077571f0e717
Koray Ozkal <koryozkal@gmail.com>
2020-04-26 23:30:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,9 +3,16 @@ class CashRegister
 attr_accessor :items, :discount, :total, :last_transaction
 
 def initialize(discount=0)
-    self.total = 0	    @total = 0
-    self.discount = discount	    @discount = discount
-    self.items = []	    @items = []
-  end	  end
+    self.total = 0	    
+    @total = 0
+    self.discount = discount	   
+    @discount = discount
+    self.items = []	  
+    @items = []
+    
+  end	 
+  end
+
+
 
 
